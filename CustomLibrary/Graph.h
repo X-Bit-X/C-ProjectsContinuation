6fdd43a4ce0d80d567e1d52015e9599b0a80c0c8
Michael Bloch <mchblch@gmail.com>
2018-09-06 19:54:16 +0200

    Minimum Spanning Tree done & finishing touches.

--- CustomLibrary/Graph.h
+++ CustomLibrary/Graph.h
@@ -12,17 +12,20 @@ class Graph
 {
 public:
 	using Node = unsigned int;
-	using Edge = std::pair<std::array<Node, 2>, double>;
+	using Edge = std::pair<double, std::array<Node, 2>>;
 
-	Graph(Node nodes, const std::vector<Edge> &edges)
-		: m_nodes{ std::move(nodes) }, m_vertercies{ std::vector<Vertex>(m_nodes) }
+	Graph() {}
+	Graph(Node nodes, std::vector<Edge> edges)
+		: m_nodes{ std::move(nodes) }, m_vertercies{ std::vector<Vertex>(m_nodes) }, m_edges{ std::move(edges) }
 	{
-		for (const auto &val : edges)
+		for (const auto &val : m_edges)
 		{
-			m_vertercies[val.first[0]].push_back({ val.second, val.first[1] });
-			m_vertercies[val.first[1]].push_back({ val.second, val.first[0] });
+			m_vertercies[val.second[0]].push_back({ val.first, val.second[1] });
+			m_vertercies[val.second[1]].push_back({ val.first, val.second[0] });
 		}
 	}
+	Graph(Node nodes)
+		: m_nodes{ std::move(nodes) }, m_vertercies{ std::vector<Vertex>(m_nodes) } {}
 
 	friend std::ostream& operator<<(std::ostream &out, const Graph &a)
 	{
@@ -42,13 +45,14 @@ public:
 	}
 
 	void setNodes(const Node &nodeLim) { m_nodes = nodeLim; m_vertercies.resize(nodeLim); }
-	void addEdge(const Edge &edge)
+	void addEdge(Edge edge)
 	{
-		const Node m = std::max(edge.first[0], edge.first[1]);
+		const Node m = std::max(edge.second[0], edge.second[1]);
 		if (m > m_vertercies.size())
 			m_vertercies.resize(m + 1);
-		m_vertercies[edge.first[0]].push_back({ edge.second, edge.first[1] });
-		m_vertercies[edge.first[1]].push_back({ edge.second, edge.first[0] });
+		m_vertercies[edge.second[0]].push_back({ edge.first, edge.second[1] });
+		m_vertercies[edge.second[1]].push_back({ edge.first, edge.second[0] });
+		m_edges.push_back(std::move(edge));
 	}
 
 	//0 -> none, 1 -> path, 2 -> circle and path
@@ -85,7 +89,7 @@ public:
 		{
 			const int current = pq.top().second;
 			pq.pop();
-			for (auto x : m_vertercies[current])
+			for (const auto &x : m_vertercies[current])
 				if (dist[x.second] > dist[current] + x.first)
 				{
 					dist[x.second] = dist[current] + x.first;
@@ -95,11 +99,32 @@ public:
 		return dist[destination];
 	}
 
+	Graph minimumSpanningTree()
+	{
+		std::sort(m_edges.begin(), m_edges.end());
+		std::vector<Node> id; id.reserve(m_nodes);
+		for (size_t i = 0; i < id.capacity(); i++)
+			id.push_back(i);
+		
+		Graph gph(m_nodes);
+		static auto root = [&](Node x) { for (; id[x] != x; x = id[x]) id[x] = id[id[x]]; return x; };
+
+		for (const auto &val : m_edges)
+		{
+			const Node x = root(val.second[0]), y = root(val.second[1]);
+			if (x != y)
+				gph.addEdge(val), id[x] = id[y];
+		}
+
+		return gph;
+	}
+
 private:
 	using Link = std::pair<double, Node>;
 	using Vertex = std::vector<Link>;
 	Node m_nodes;
 	std::vector<Vertex> m_vertercies;
+	std::vector<Edge> m_edges;
 
 	std::vector<bool> goThrough(std::vector<bool> check, const Node &node = 0) const
 	{
